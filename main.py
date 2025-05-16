0bff792cf85c5aa78a7018e6ae02171aa23e615e
ReiTony <reimerstony@gmail.com>
2025-05-16 14:53:53 +0800

    refactor: clean up unused routes, update .gitignore and requirements for local testing
    - Removed unused routes from main.py
    - Updated .gitignore to include the test folder
    - Removed unnecessary libraries from requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-from app.api.user_route import router as auth_router
-from app.api.pet_profile_route import router as pet_router
-from app.api.chat_route import router as chat_router
+# from app.api.chat_route import router as chat_router
+from app.api.test_chat_route import router as chat_router
+
 
 # FastAPI App Initialization
 app = FastAPI(
@@ -11,14 +13,25 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Routers
-app.include_router(auth_router, prefix="/api/auth", tags=["User Creation"])
-app.include_router(pet_router, prefix="/api/pet", tags=["Pet Profile"])
-app.include_router(chat_router, prefix="/api", tags=["Chat"])
+# app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
+app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
 
 # Health Endpoints
 @app.get("/")
 async def root():
     
     return {"message": "Server is running"}
+
+# Run the App
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=1234, log_level="info")
 
